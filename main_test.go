e238f448a971025b3bdb789357783e389d03a968
emil64 <emil@bratianu.science>
2019-11-23 07:48:09 +0000

    stage 3 was already done, just added some tests

--- main_test.go
+++ main_test.go
@@ -159,6 +159,71 @@ func Test(t *testing.T) {
 			},
 		}},
 
+		{"16x16x16-100", args{
+			p: golParams{
+				turns:       100,
+				threads:     8,
+				imageWidth:  16,
+				imageHeight: 16,
+			},
+			expectedAlive: []cell{
+				{x: 12, y: 0},
+				{x: 13, y: 0},
+				{x: 14, y: 0},
+				{x: 13, y: 14},
+				{x: 14, y: 15},
+			},
+		}},
+
+		{"16x16x6-100", args{
+			p: golParams{
+				turns:       100,
+				threads:     8,
+				imageWidth:  16,
+				imageHeight: 16,
+			},
+			expectedAlive: []cell{
+				{x: 12, y: 0},
+				{x: 13, y: 0},
+				{x: 14, y: 0},
+				{x: 13, y: 14},
+				{x: 14, y: 15},
+			},
+		}},
+
+
+		{"16x16x28-100", args{
+			p: golParams{
+				turns:       100,
+				threads:     8,
+				imageWidth:  16,
+				imageHeight: 16,
+			},
+			expectedAlive: []cell{
+				{x: 12, y: 0},
+				{x: 13, y: 0},
+				{x: 14, y: 0},
+				{x: 13, y: 14},
+				{x: 14, y: 15},
+			},
+		}},
+
+		{"16x16x180-100", args{
+			p: golParams{
+				turns:       100,
+				threads:     8,
+				imageWidth:  16,
+				imageHeight: 16,
+			},
+			expectedAlive: []cell{
+				{x: 12, y: 0},
+				{x: 13, y: 0},
+				{x: 14, y: 0},
+				{x: 13, y: 14},
+				{x: 14, y: 15},
+			},
+		}},
+
 		// Special test to be used to generate traces - not a real test
 		//{"trace", args{
 		//	p: golParams{
